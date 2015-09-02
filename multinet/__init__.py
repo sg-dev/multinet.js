2ff277d3029d934efe0aab91ff584e5baed59be1
Florian Hahn <flo@fhahn.com>
2015-09-02 12:00:49 +0200

    Handle edge list uploads

--- multinet/__init__.py
+++ multinet/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = '/tmp'
 
 VISUALIZATION_DIR = os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
--- multinet/views.py
+++ multinet/views.py
@@ -1,8 +1,14 @@
-from flask import render_template
-from flask import jsonify
+import os
+
+from flask import render_template, jsonify, Flask, request, redirect, url_for
+
+from werkzeug import secure_filename
 
 from multinet import app, VISUALIZATION_DIR
 from multinet.render import graph_layout
+
+
+ALLOWED_EXTENSIONS = set(['csv',])
 
 
 @app.route('/')
@@ -14,6 +20,24 @@ def main():
 def sample_visuals_np(dataset):
         filename = "%s/%s.csv" % ( VISUALIZATION_DIR, dataset)  
         nd_filename = "%s/%s_node_data.csv" % ( VISUALIZATION_DIR, dataset)
-        data = graph_layout(filename, nd_filename, True) 
+        data = graph_layout(filename, nd_filename) 
 
         return jsonify(**data)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload/', methods=['POST',])
+def upload_file():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(path)
+        data = graph_layout(path, None)
+        return jsonify(**data)
+
+    return jsonify(error='Invalid fileupload')

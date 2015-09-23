549a5091be7ca9c42135940b9d49a1169b855017
Emre Sarigol <esarigol@web.sg.ethz.ch>
2015-09-23 13:58:38 +0200

    Update style

--- multinet/static/js/multinet.js
+++ multinet/static/js/multinet.js
@@ -767,8 +767,47 @@ function createGraph(data, renderData, coordinateTransformer, doAnimate, degreeS
     $( "#year" ).val( data.unique_keys[0] + " - " + data.unique_keys[data.unique_keys.length - 1]);
 
     for (var i = 0; i < data.layer_ct; i++) {
-        $("#info-container .layer-selection ul.dropdown-menu").append('<li role="presentation"><a role="menuitem" tabindex="-1" href="#" onclick="displayLayerInfo('+i+'); return 0;">Layer '+(i+1)+'</a></li>');
+        $("#info-container .layer-selection ul.dropdown-menu-layers").append('<li role="presentation"><a role="menuitem" tabindex="-1" href="#" onclick="displayLayerInfo('+i+'); return 0;">Layer '+(i+1)+'</a></li>');
     }
+    
+    
+    var scales = ['In Degree', 'Out Degree', 'Total Degree'];
+    $.each( scales , function( i, val ) { 
+    	var el = '<li role="presentation"><a role="menuitem" tabindex="-1" href="#" onclick="scaleNodes(';
+    	el += "'" + val + "'";
+    	el += '); return 0;">';
+    	el += val + '</a></li>';
+    	$("ul.dropdown-menu-scale").append(el);
+    });
+    
+    var replayModes = ['single','window']
+    $.each( replayModes , function( i, val ) { 
+    	
+    	if(val == "window"){
+    		var modeName = "Window"
+    	}else{
+    		var modeName = "Fixed Start"
+    	}
+    	
+    	var el = '<li role="presentation"><a role="menuitem" tabindex="-1" href="#" onclick="setReplayMode(';
+    	el += "'" + val + "'";
+    	el += '); return 0;">';
+    	el += modeName + '</a></li>';
+    	$("ul.dropdown-menu-replaymode").append(el);
+    });
+    
+    
+	var replaySpeeds = [1,2,4]
+    $.each( replaySpeeds , function( i, val ) { 
+    	
+    	var speedName = val + "x";
+    	
+    	var el = '<li role="presentation"><a role="menuitem" tabindex="-1" href="#" onclick="setReplaySpeed(';
+    	el += val;
+    	el += '); return 0;">';
+    	el += speedName + '</a></li>';
+    	$("ul.dropdown-menu-replayspeed").append(el);
+    });
 
     var oldLayer = 0;
     var layerCameraPos = {0: 0};
@@ -858,7 +897,24 @@ function createGraph(data, renderData, coordinateTransformer, doAnimate, degreeS
             }
         }
     }
+    
+    setReplayMode = function(mode){
+    	
+    	$("#replay-mode").val(mode);
+    	if(mode=="window"){
+    		$("#ReplayModeMenu").text("Window");
+    	}else{
+    		$("#ReplayModeMenu").text("Fixed Start");
+    	}
+    }
 
+    setReplaySpeed = function(speed){
+    	
+    	$("#replay-speed").val(speed);
+    	var txt = speed + "x";
+    	$("#ReplaySpeedMenu").text(txt);
+    	
+    }
 
     $('#graph-replay').click(function(e) {
 
@@ -1062,12 +1118,14 @@ function highlightNeighbors(neighborhood_geometry, highlight_geom, graphData, no
     });
 }
 
-function showPopup(x, y, data, labels) {
-    var table = '<table class="table table-striped" style="margin-bottom: 0 !important;">';
+function showPopup(x, y, node_id, data, labels) {
+    var table = '<table class="table table-striped" style="margin-bottom: 0 !important;width:auto !important;">';
     if (labels.length == 0) {
-        return;
-    }
-
+        return; 
+    } 
+	
+    table += '<tr>\n<td>ID</td>\n<td>'+ node_id +'</td></tr>';
+    
     $.each(data, function(i, obj) {
         table += '<tr>\n<td>'+labels[i+1]+'</td>\n<td>'+obj+'</td></tr>';
     });
@@ -1156,7 +1214,7 @@ function makeOnMouseDownHandler(renderData, graphData) {
             $("#clear-selection-tr").show();
             var selected_node = nearest_intersection.object;
 
-            showPopup(event.clientX, event.clientY, graphData.node_data[selected_node.node_id], graphData.data_labels);
+            showPopup(event.clientX, event.clientY, selected_node.node_id, graphData.node_data[selected_node.node_id], graphData.data_labels);
 
             update_scene = true;
 
